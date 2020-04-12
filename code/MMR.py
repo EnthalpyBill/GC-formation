1264611d48efedccabd5b447bf59c2a94336b9bd
Bill <billcyt@pku.edu.cn>
2020-04-12 17:02:38 +0800

    updated a lot

--- code/MMR.py
+++ code/MMR.py
@@ -13,10 +13,12 @@ import numpy as np
 
 def mmr_lg14(m_star, z):
     n = 2.8
-    return 0.4*np.log10(m_star / (10**10.5)) - (0.216*n*np.log10(1+z))
+    mmr = 0.4*np.log10(m_star / (10**10.5)) - (0.216*n*np.log10(1+z))
+    return mmr + (0.2-mmr)*np.array(mmr>0.2, dtype=np.int)
 
 
 # ***** MMR in Choksi et al. (2018) *****
 
 def mmr_cgl18(m_star, z):
-    return 0.35*np.log10(m_star / (10**10.5)) - (0.9*np.log10(1+z))
+    mmr = 0.35*np.log10(m_star / (10**10.5)) - (0.9*np.log10(1+z))
+    return mmr + (0.3-mmr)*np.array(mmr>0.3, dtype=np.int)
--- code/plot_SMHM.py
+++ code/plot_SMHM.py
@@ -10,14 +10,19 @@ By Bill Chen
 import numpy as np
 from SMHM import log_smhm_b13
 import BSG_ratio as bsg
+import MMR as mmr
 import matplotlib.pyplot as plt
 
 log_mh = np.linspace(9.5, 14, 100+1)
+mh = 10**log_mh
 z = np.array([0, 1, 3, 5])
 c = ["k", "r", "b", "g"]
 
 for i in range(len(z)):
-    plt.plot(log_mh, np.log10(bsg.star_ratio_lg14(10**log_mh, z[i])), c=c[i], ls="--")
-    plt.plot(log_mh, np.log10(bsg.gas_ratio_cgl18(10**log_mh, z[i])), c=c[i], ls="-")
+    plt.plot(mh, bsg.star_ratio_lg14(mh, z[i]), c=c[i], ls="--")
+    # plt.plot(mh, bsg.gas_ratio_lg14(mh, z[i]), c=c[i], ls=":")
+    plt.plot(mh, bsg.gas_ratio_cgl18(mh, z[i]), c=c[i], ls="-")
 
+plt.xscale('log')
+plt.yscale('log')
 plt.show()
